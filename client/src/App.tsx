dac689f2ef42fdf30889da24b7257119600abe2a
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-09-07 02:55:03 +0530

    Resturent Frontend template setup

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button>Hii</Button>
+    </>
+  );
+}
+
+export default App;
