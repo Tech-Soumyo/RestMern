9ef66431dc3bf4e69691c536d6d9bb32c609aca8
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-10-08 01:12:03 +0530

    login

---
+++ client/src/auth/login.tsx
@@ -0,0 +1,117 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { LoginInputState, userLoginSchema } from "@/schema/user.schema";
+import { Eye, Loader2, Mail } from "lucide-react";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { Link } from "react-router-dom";
+
+function Login() {
+  const [input, setInput] = useState<LoginInputState>({
+    email: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState<Partial<LoginInputState>>({});
+  //  const { loading, login } = useUserStore();
+
+  const changeEventHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setInput({ ...input, [name]: value });
+  };
+
+  const loginSubmitHandler = (e: FormEvent) => {
+    e.preventDefault();
+    const result = userLoginSchema.safeParse(input);
+    if (!result.success) {
+      const fieldErrors = result.error.formErrors.fieldErrors;
+      setErrors(fieldErrors as Partial<LoginInputState>);
+      return;
+    }
+    // try {
+    //   await login(input)
+    // } catch (error) {
+    //   console.error(error, "Error during Login User")
+    // }
+    console.log(input);
+  };
+
+  const loading = false;
+  return (
+    <div className="flex items-center justify-center h-screen min-h-screen">
+      <form
+        onSubmit={loginSubmitHandler}
+        className="md:p-8 w-full max-w-md md:border border-gray-200 rounded-lg mx-4"
+      >
+        <div className="mb-4">
+          <h1 className="font-bold text-2xl">RestMern</h1>
+        </div>
+        <div className="mb-4">
+          <div className="relative">
+            <Input
+              type="email"
+              name="email"
+              placeholder=" Enter Your Email"
+              value={input.email}
+              onChange={changeEventHandler}
+              className="pl-10 focus-visible:ring-0"
+            />
+            <Mail className="absolute inset-y-1.5 left-2 text-gray-500 pointer-events-none" />
+            {errors && (
+              <span className="text-xs text-red-500">{errors.email}</span>
+            )}
+          </div>
+        </div>
+        <div className="mb-4">
+          <div className="relative">
+            <Input
+              type="password"
+              name="password"
+              placeholder=" Enter Your Password"
+              value={input.password}
+              onChange={changeEventHandler}
+              className="pl-10 focus-visible:ring-0"
+            />
+            <Eye className="absolute inset-y-1.5 left-2 text-gray-500 pointer-events-none" />
+            {errors && (
+              <span className="text-xs text-red-500">{errors.password}</span>
+            )}
+          </div>
+        </div>
+        <div>
+          {loading ? (
+            <Button
+              disabled
+              className="bg-blue-500 hover:bg-hoverOrange w-full"
+            >
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please Wait
+            </Button>
+          ) : (
+            <Button
+              type="submit"
+              className="bg-blue-500 hover:bg-hoverOrange w-full"
+            >
+              Login
+            </Button>
+          )}
+          <div className="mt-4">
+            <Link
+              to="/forgotpassword"
+              className="hover:text-blue-500 hover:underline"
+            >
+              Forgot Password
+            </Link>
+          </div>
+        </div>
+        <Separator />
+        <p className="mt-2">
+          Don't have an account?{" "}
+          <Link to="/signup" className="text-blue-500">
+            Signup
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
