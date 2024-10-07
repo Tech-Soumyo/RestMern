b7ad4cfc0ce0cbebfdc98f99cdeedc30269174c7
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-10-08 01:10:04 +0530

    add paths and routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,60 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
-import { Button } from "./components/ui/button";
+import Login from "./auth/login";
+// import MainLayout from "./MainLayout";
+import SignUp from "./auth/SignUp";
+import ForgotPassword from "./auth/forgotPassword";
+import ResetPassword from "./auth/ResetPassword";
+import VerifyEmail from "./auth/VerifyEmail";
+// import Navbar from "./components/custom/Navbar";
+import HereSection from "./components/custom/HeroSection";
+import MainLayout from "./layout/MainLayout";
+// import Profile from "./components/custom/Profile";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HereSection />,
+      },
+      // {
+      //   path: "/profile",
+      //   element: <Profile />,
+      // },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+  {
+    path: "/forgotPassword",
+    element: <ForgotPassword />,
+  },
+  {
+    path: "/resetPassword",
+    element: <ResetPassword />,
+  },
+  {
+    path: "/verifyEmail",
+    element: <VerifyEmail />,
+  },
+]);
 
 function App() {
   return (
     <>
-      <Button>Hii</Button>
+      <main>
+        <RouterProvider router={appRouter}></RouterProvider>
+        {/* <Login /> */}
+      </main>
     </>
   );
 }
