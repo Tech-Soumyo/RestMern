592646afec5bef4f98d4b8ef0fc4a36ad0e79618
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-11-16 14:04:59 +0530

    Cart, Resturent, Menu, Orders, UserOrder component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,11 +7,16 @@ import ForgotPassword from "./auth/forgotPassword";
 import ResetPassword from "./auth/ResetPassword";
 import VerifyEmail from "./auth/VerifyEmail";
 // import Navbar from "./components/custom/Navbar";
-import HereSection from "./components/custom/HeroSection";
+import HereSection from "./components/custom/user/HeroSection";
 import MainLayout from "./layout/MainLayout";
-import Profile from "./components/custom/Profile";
-import Search from "./components/custom/SearchPage";
-import { ResturentDetails } from "./components/custom/ResturentDetails";
+import Profile from "./components/custom/user/Profile";
+import Search from "./components/custom/user/SearchPage";
+import { ResturentDetails } from "./components/custom/user/ResturentDetails";
+import Cart from "./components/custom/user/Cart";
+import Resturent from "./components/custom/admin/Resturent";
+import Menu from "./components/custom/admin/Menu";
+import Orders from "./components/custom/admin/Order";
+import UserOrder from "./components/custom/user/UserOrder";
 
 const appRouter = createBrowserRouter([
   {
@@ -33,6 +38,32 @@ const appRouter = createBrowserRouter([
       {
         path: "/restaurant/:id",
         element: <ResturentDetails />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/status",
+        element: <UserOrder />,
+      },
+      //  {
+      //   path: "/order/status",
+      //   element: <Success />,
+      // },
+
+      // Admin page
+      {
+        path: "/admin/resturent",
+        element: <Resturent />,
+      },
+      {
+        path: "/admin/menu",
+        element: <Menu />,
+      },
+      {
+        path: "/admin/orders",
+        element: <Orders />,
       },
     ],
   },
