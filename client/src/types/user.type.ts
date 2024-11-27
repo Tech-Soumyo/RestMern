d4c8ce2635f0ae0d3a8e22bfd6c37e09583f2a24
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-11-27 15:16:27 +0530

    types

---
+++ client/src/types/user.type.ts
@@ -0,0 +1,28 @@
+import { LoginInputState, SignupInputState } from "@/schema/user.schema";
+
+export type User = {
+  fullName: string;
+  email: string;
+  contact: number;
+  address: string;
+  city: string;
+  country: string;
+  profilePicture: string;
+  admin: boolean;
+  isVerified: boolean;
+};
+
+export type UserState = {
+  user: User | null;
+  isAuthenticated: boolean;
+  isCheckingAuth: boolean;
+  loading: boolean;
+  signup: (input: SignupInputState) => Promise<void>;
+  login: (input: LoginInputState) => Promise<void>;
+  verifyEmail: (verificationCode: string) => Promise<void>;
+  checkAuthentication: () => Promise<void>;
+  logout: () => Promise<void>;
+  forgotPassword: (email: string) => Promise<void>;
+  resetPassword: (token: string, newPassword: string) => Promise<void>;
+  updateProfile: (input: any) => Promise<void>;
+};
