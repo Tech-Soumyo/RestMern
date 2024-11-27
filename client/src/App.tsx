07d8fd521e68194d8953475da22d5ecd69cdde18
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-11-27 15:01:20 +0530

    protect the routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,8 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 import "./App.css";
 import Login from "./auth/login";
 // import MainLayout from "./MainLayout";
@@ -7,21 +11,74 @@ import ForgotPassword from "./auth/forgotPassword";
 import ResetPassword from "./auth/ResetPassword";
 import VerifyEmail from "./auth/VerifyEmail";
 // import Navbar from "./components/custom/Navbar";
-import HereSection from "./components/custom/user/HeroSection";
+import HereSection from "./components/custom/CommonCompoNents/HeroSection";
 import MainLayout from "./layout/MainLayout";
-import Profile from "./components/custom/user/Profile";
-import Search from "./components/custom/user/SearchPage";
-import { ResturentDetails } from "./components/custom/user/ResturentDetails";
-import Cart from "./components/custom/user/Cart";
+import Profile from "./components/custom/CommonCompoNents/Profile";
+import Search from "./components/custom/CommonCompoNents/SearchPage";
+import { ResturentDetails } from "./components/custom/CommonCompoNents/ResturentDetails";
+import Cart from "./components/custom/CommonCompoNents/Cart";
 import Resturent from "./components/custom/admin/Resturent";
 import Menu from "./components/custom/admin/Menu";
 import Orders from "./components/custom/admin/Order";
-import UserOrder from "./components/custom/user/UserOrder";
+import UserOrder from "./components/custom/CommonCompoNents/UserOrder";
+import React, { useEffect } from "react";
+import { useUserStore } from "./zustandStore/useUserStore";
+import { useThemeStore } from "./zustandStore/themeStore";
+import Loading from "./components/custom/CommonCompoNents/Loading";
+
+const ProtectedRoutes = ({ children }: { children: React.ReactNode }) => {
+  // Zustand User Store
+  const { isAuthenticated, user, isCheckingAuth } = useUserStore();
+
+  if (isCheckingAuth) {
+    return <div>Loading...</div>; // Or a proper loading component
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (user?.isVerified) {
+    return <Navigate to="/" replace />;
+  }
+
+  if (user?.isVerified == false || undefined) {
+    return <Navigate to="/verify-email" replace />;
+  }
+
+  return children;
+};
+
+const AuthenticatedUser = ({ children }: { children: React.ReactNode }) => {
+  // Zustand User Store
+  const { isAuthenticated, user } = useUserStore();
+  if (isAuthenticated && user?.isVerified) {
+    return <Navigate to="/" replace />;
+  }
+  return children;
+};
+
+const AdminRoute = ({ children }: { children: React.ReactNode }) => {
+  // Zustand User Store
+  const { user, isAuthenticated } = useUserStore();
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+  if (!user?.admin) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+};
 
 const appRouter = createBrowserRouter([
   {
     path: "/",
-    element: <MainLayout />,
+    element: (
+      <ProtectedRoutes>
+        <MainLayout />
+      </ProtectedRoutes>
+    ),
     children: [
       {
         path: "/",
@@ -54,42 +111,77 @@ const appRouter = createBrowserRouter([
 
       // Admin page
       {
-        path: "/admin/resturent",
-        element: <Resturent />,
+        path: "/admin/restaurant",
+        element: (
+          <AdminRoute>
+            <Resturent />
+          </AdminRoute>
+        ),
       },
       {
         path: "/admin/menu",
-        element: <Menu />,
+        element: (
+          <AdminRoute>
+            <Menu />
+          </AdminRoute>
+        ),
       },
       {
         path: "/admin/orders",
-        element: <Orders />,
+        element: (
+          <AdminRoute>
+            <Orders />
+          </AdminRoute>
+        ),
       },
     ],
   },
   {
     path: "/login",
-    element: <Login />,
+    element: (
+      <AuthenticatedUser>
+        <Login />
+      </AuthenticatedUser>
+    ),
   },
   {
     path: "/signup",
-    element: <SignUp />,
+    element: (
+      <AuthenticatedUser>
+        <SignUp />
+      </AuthenticatedUser>
+    ),
   },
   {
-    path: "/forgotPassword",
-    element: <ForgotPassword />,
+    path: "/forgot-password",
+    element: (
+      <AuthenticatedUser>
+        <ForgotPassword />
+      </AuthenticatedUser>
+    ),
   },
   {
-    path: "/resetPassword",
+    path: "/reset-password",
     element: <ResetPassword />,
   },
   {
-    path: "/verifyEmail",
+    path: "/verify-email",
     element: <VerifyEmail />,
   },
 ]);
 
 function App() {
+  // Zustand Theme Store
+  const initializeTheme = useThemeStore((state: any) => state.initializeTheme);
+  // Zustand User Store
+  const { checkAuthentication, isCheckingAuth } = useUserStore();
+  // checking auth every time when page is loaded
+  useEffect(() => {
+    checkAuthentication();
+    initializeTheme();
+  }, [checkAuthentication]);
+
+  if (isCheckingAuth) return <Loading />;
   return (
     <>
       <main>
