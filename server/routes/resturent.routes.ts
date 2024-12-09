982c71884aea79fa5a02034ac16e4ef3a8452f6a
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-12-10 01:13:12 +0530

    restaurant routes

---
+++ server/routes/resturent.routes.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+import { isAuthenticated } from "../middleware/isAuthenticated.mid";
+import { upload } from "../middleware/multer";
+import {
+  createRestaurant,
+  getRestaurant,
+  getRestaurantOrder,
+  getSingleRestaurant,
+  searchRestaurant,
+  updateOrderStatus,
+  updateRestaurant,
+} from "../controller/resturent.controller";
+
+const router = express.Router();
+router.use(bodyParser.json());
+router
+  .route("/")
+  .post(isAuthenticated, upload.single("imageFile"), createRestaurant);
+router.route("/").get(isAuthenticated, getRestaurant);
+router
+  .route("/")
+  .put(isAuthenticated, upload.single("imageFile"), updateRestaurant);
+router.route("/order").get(isAuthenticated, getRestaurantOrder);
+router.route("/order/:orderId/status").put(isAuthenticated, updateOrderStatus);
+router.route("/search/:searchText").get(isAuthenticated, searchRestaurant);
+router.route("/:id").get(isAuthenticated, getSingleRestaurant);
+
+export default router;
