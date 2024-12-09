0e35f77415bf899412ea421ff89b14a1281ce868
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-12-09 23:32:42 +0530

    implement cart funtionalities

--- client/src/components/custom/CommonCompoNents/Cart.tsx
+++ client/src/components/custom/CommonCompoNents/Cart.tsx
@@ -21,7 +21,13 @@ import { CartItem } from "@/types/cart.type";
 
 const Cart = () => {
   const [open, setOpen] = useState<boolean>(false);
-  const { cart, decrementQuantity, incrementQuantity } = useCartStore();
+  const {
+    cart,
+    decrementQuantity,
+    incrementQuantity,
+    clearCart,
+    removeFromTheCart,
+  } = useCartStore();
 
   let totalAmount = cart.reduce((acc, ele) => {
     return acc + ele.price * ele.quantity;
@@ -32,7 +38,14 @@ const Cart = () => {
   return (
     <div className="flex flex-col max-w-7xl mx-auto my-10">
       <div className="flex justify-end">
-        <Button variant="link">Clear All</Button>
+        <Button
+          variant="link"
+          onClick={() => {
+            clearCart();
+          }}
+        >
+          Clear All
+        </Button>
       </div>
       <Table>
         <TableHeader>
@@ -59,7 +72,10 @@ const Cart = () => {
               <TableCell>
                 <div className="w-fit flex items-center rounded-full border border-gray-100 dark:border-gray-800 shadow-md">
                   <Button
-                    onClick={() => decrementQuantity(item._id)}
+                    onClick={() => {
+                      decrementQuantity(item._id);
+                      // console.log("remove");
+                    }}
                     size={"icon"}
                     variant={"outline"}
                     className="rounded-full bg-gray-200"
@@ -86,7 +102,13 @@ const Cart = () => {
               </TableCell>
               <TableCell>{item.price * item.quantity}</TableCell>
               <TableCell className="text-right">
-                <Button size={"sm"} className="bg-orange hover:bg-hoverOrange">
+                <Button
+                  size={"sm"}
+                  className="bg-orange hover:bg-hoverOrange"
+                  onClick={() => {
+                    removeFromTheCart(item._id);
+                  }}
+                >
                   Remove
                 </Button>
               </TableCell>
@@ -97,6 +119,13 @@ const Cart = () => {
           <TableRow className="text-2xl font-bold">
             <TableCell colSpan={5}>Total</TableCell>
             <TableCell className="text-right"> {totalAmount}</TableCell>
+            {/* <button
+              onClick={() => {
+                console.log(totalAmount);
+              }}
+            >
+              totalAmount
+            </button> */}
           </TableRow>
         </TableFooter>
       </Table>
