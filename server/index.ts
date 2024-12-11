be045439de533cd413fc93bbfb8a7b06987a3655
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-12-11 10:33:07 +0530

    re deploy

--- server/index.ts
+++ server/index.ts
@@ -20,29 +20,33 @@ console.log(PORT);
 const DIRNAME = path.resolve();
 
 // default middleware for any mern project
-app.use(bodyParser.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
-app.use(express.json());
-app.use(cookieParser());
-const corsOptions = {
-  origin: "https://restmern.onrender.com",
-  credentials: true,
-};
-app.use(cors(corsOptions));
-//localhost:3000/api/v1/order/checkout/create-checkout-session
-// api
-http: app.use("/api/v1/user", userRoute);
-app.use("/api/v1/restaurant", restaurantRoute);
-app.use("/api/v1/menu", menuRoute);
-// app.use(express.json());
-app.use("/api/v1/order", express.json(), orderRoute);
+try {
+  app.use(bodyParser.json({ limit: "10mb" }));
+  app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+  app.use(express.json());
+  app.use(cookieParser());
+  const corsOptions = {
+    origin: "http://localhost:3000",
+    credentials: true,
+  };
+  app.use(cors(corsOptions));
+  //localhost:3000/api/v1/order/checkout/create-checkout-session
+  // api
+  http: app.use("/api/v1/user", userRoute);
+  app.use("/api/v1/restaurant", restaurantRoute);
+  app.use("/api/v1/menu", menuRoute);
+  // app.use(express.json());
+  app.use("/api/v1/order", express.json(), orderRoute);
 
-app.use(express.static(path.join(DIRNAME, "/client/dist")));
-app.use("*", (_, res) => {
-  res.sendFile(path.resolve(DIRNAME, "client", "dist", "index.html"));
-});
+  app.use(express.static(path.join(DIRNAME, "/client/dist")));
+  app.use("*", (_, res) => {
+    res.sendFile(path.resolve(DIRNAME, "client", "dist", "index.html"));
+  });
 
-app.listen(PORT, () => {
-  connectDB();
-  console.log(`Server listen at port ${PORT}`);
-});
+  app.listen(PORT, () => {
+    connectDB();
+    console.log(`Server listen at port ${PORT}`);
+  });
+} catch (error) {
+  console.log(error);
+}
