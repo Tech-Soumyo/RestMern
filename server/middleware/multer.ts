18b8ef94e743bc50b4bf89badcfeabebdaf7c82a
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-12-21 21:32:11 +0530

    change name

--- server/middleware/multer.ts
+++
@@ -1,9 +0,0 @@
-import multer from "multer";
-
-const storage = multer.memoryStorage();
-export const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 5 * 1024 * 1024, // 5mb
-  },
-});
